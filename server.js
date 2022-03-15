b8cecf2e07fa74eec71473472051da076f8e0f75
koukol-p <p.koukol13@gmail.com>
2022-03-15 11:35:58 +0100

    fixed mixed content error

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "build");
 const port = process.env.PORT || 3000;
+app.use(function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "https") {
+    res.redirect("http://" + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static(publicPath));
 
